8c31133327c2711f2d81f5d35cd8bbd54e667a75
Michail Plushnikov <michail@plushnikov.de>
2013-11-03 22:36:22 +0100

    backport registration of LombokLightMethodTreeGenerator

--- lombok-plugin/src/main/java/de/plushnikov/intellij/plugin/LombokLoader.java
+++ lombok-plugin/src/main/java/de/plushnikov/intellij/plugin/LombokLoader.java
@@ -2,6 +2,8 @@ package de.plushnikov.intellij.plugin;
 
 import com.intellij.openapi.components.ApplicationComponent;
 import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.psi.impl.source.tree.ChangeUtil;
+import de.plushnikov.intellij.plugin.extension.LombokLightMethodTreeGenerator;
 import org.jetbrains.annotations.NotNull;
 
 /**
@@ -19,6 +21,7 @@ public class LombokLoader implements ApplicationComponent {
   @Override
   public void initComponent() {
     LOG.info("Lombok plugin initialized for IntelliJ");
+    ChangeUtil.registerTreeGenerator(new LombokLightMethodTreeGenerator());
   }
 
   @Override
