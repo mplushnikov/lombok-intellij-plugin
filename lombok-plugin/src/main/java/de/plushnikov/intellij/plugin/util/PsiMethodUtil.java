0687fde6142f1fd16564fd307384c092704c91b7
Michail Plushnikov <michail@plushnikov.de>
2014-02-16 17:10:34 +0100

    merged from master

--- lombok-plugin/src/main/java/de/plushnikov/intellij/plugin/util/PsiMethodUtil.java
+++ lombok-plugin/src/main/java/de/plushnikov/intellij/plugin/util/PsiMethodUtil.java
@@ -39,40 +39,6 @@ public class PsiMethodUtil {
     return elementFactory.createCodeBlockFromText("{" + blockText + "}", psiClass);
   }
 
-  public static PsiTypeParameterList createTypeParameterList(@NotNull PsiTypeParameterList psiTypeParameterList) {
-    PsiTypeParameter[] psiTypeParameters = psiTypeParameterList.getTypeParameters();
-    if (psiTypeParameters.length > 0) {
-
-      final PsiElementFactory elementFactory = JavaPsiFacade.getInstance(psiTypeParameterList.getProject()).getElementFactory();
-
-      final StringBuilder builder = new StringBuilder("public <");
-
-      for (PsiTypeParameter psiTypeParameter : psiTypeParameters) {
-        builder.append(psiTypeParameter.getName());
-
-        PsiClassType[] superTypes = psiTypeParameter.getExtendsListTypes();
-        if (superTypes.length > 1 || superTypes.length == 1 && !superTypes[0].equalsToText(CommonClassNames.JAVA_LANG_OBJECT)) {
-          builder.append(" extends ");
-          for (PsiClassType type : superTypes) {
-            if (type.equalsToText(CommonClassNames.JAVA_LANG_OBJECT)) {
-              continue;
-            }
-            builder.append(type.getCanonicalText()).append('&');
-          }
-          builder.deleteCharAt(builder.length() - 1);
-        }
-        builder.append(',');
-      }
-      builder.deleteCharAt(builder.length() - 1);
-
-      builder.append("> void foo(){}");
-
-      PsiMethod methodFromText = elementFactory.createMethodFromText(builder.toString(), null);
-      return methodFromText.getTypeParameterList();
-    }
-    return null;
-  }
-
   @Nullable
   public static PsiTypeParameterList createTypeParameterList(@NotNull PsiTypeParameterList psiTypeParameterList) {
     PsiTypeParameter[] psiTypeParameters = psiTypeParameterList.getTypeParameters();
