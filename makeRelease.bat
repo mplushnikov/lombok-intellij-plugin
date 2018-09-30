d78d6ba3b3236cb4a631b6fac9f1312f852981f6
Michail Plushnikov <mplushnikov@gmail.com>
2018-09-30 16:51:41 +0200

    merged for 2016.2 and 2016.3 with Java 8 support

--- makeRelease.bat
+++ makeRelease.bat
@@ -2,14 +2,14 @@
 
 SET pluginVersion=0.20
 
-for %%X in (2016.1 2016.2 2016.3) do call :buildPlugin %%X
+for %%X in (2016.2 2016.3) do call :buildPlugin %%X
 ECHO All Done
 GOTO :eof
 
 echo.&pause&goto:eof
 
 :buildPlugin
-SETLOCALj
+SETLOCAL
 echo Called with %1
 SET IDEA_VERSION=%1
 call gradlew clean
