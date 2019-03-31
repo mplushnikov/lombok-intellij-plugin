9bec1806c38169ff52ef83c32bc858345c87d4c7
Michail Plushnikov <mplushnikov@gmail.com>
2019-03-31 21:57:11 +0200

    deactivated val inspection test

--- testData/inspection/diverse/ValInspection.java
+++ testData/inspection/diverse/ValInspection.java
@@ -11,10 +11,22 @@ public class ValInspection {
 
     val d = System.getProperty("sss");
 
-    // 'val' is not allowed in old-style for loops
-    for (<error descr = "'val' is not allowed in old-style for loops"> val i = 0;</error> i < 10; i++){
-      val j = 2;
-    }
+    // 'val' is not compatible with array initializer expressions. Use the full form (new int[] { ... } instead of just { ... })
+    <error descr="'val' is not compatible with array initializer expressions. Use the full form (new int[] { ... } instead of just { ... })">val e = <error descr="Array initializer is not allowed here">{"xyz"}</error>;</error>
 
+    // 'val' is not allowed with lambda expressions.
+    <error descr="'val' is not allowed with lambda expressions.">val f = <error descr="<lambda expression> is not a functional interface">() -> "xyz"</error>;</error>
+
+    // 'val' on a local variable requires an initializer expression
+    <error descr="'val' on a local variable requires an initializer expression">val g;</error>
+
+    for (val h : <error descr="Cannot resolve symbol 'Arrays'">Arrays</error>.asList("a", "b", "c")) {
+      System.out.println(h);
+    }
+  }
+
+  public void test(<error descr="'val' works only on local variables and on foreach loops">val x</error>) {
+    String val = "val";
+    System.out.println(val);
   }
 }
