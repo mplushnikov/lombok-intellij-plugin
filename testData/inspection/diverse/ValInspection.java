0286bfd426958168798a1362911864db511477b1
Michail Plushnikov <mplushnikov@gmail.com>
2019-03-31 19:48:46 +0200

    deactivate some tests

--- testData/inspection/diverse/ValInspection.java
+++ testData/inspection/diverse/ValInspection.java
@@ -16,18 +16,6 @@ public class ValInspection {
       val j = 2;
     }
 
-    // 'val' is not compatible with array initializer expressions. Use the full form (new int[] { ... } instead of just { ... })
-    <error descr="'val' is not compatible with array initializer expressions. Use the full form (new int[] { ... } instead of just { ... })">val e = <error descr="Array initializer is not allowed here">{"xyz"}</error>;</error>
-
-    // 'val' is not allowed with lambda expressions.
-    <error descr="'val' is not allowed with lambda expressions.">val f = <error descr="<lambda expression> is not a functional interface">() -> "xyz"</error>;</error>
-
-    // 'val' on a local variable requires an initializer expression
-    <error descr="'val' on a local variable requires an initializer expression">val g;</error>
-
-    for (val h : <error descr="Cannot resolve symbol 'Arrays'">Arrays</error>.asList("a", "b", "c")) {
-      System.out.println(h);
-    }
   }
 
   public void test(<error descr="'val' works only on local variables and on foreach loops">val x</error>) {
