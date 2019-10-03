3f685ef2bec41e2b411dafdb75c6df2e6ddb0f5f
Michail Plushnikov <mplushnikov@gmail.com>
2019-10-03 16:20:32 +0200

    merged with backported changes from master

--- src/test/java/de/plushnikov/intellij/plugin/LombokTestUtil.java
+++ src/test/java/de/plushnikov/intellij/plugin/LombokTestUtil.java
@@ -1,7 +1,5 @@
 package de.plushnikov.intellij.plugin;
 
-import com.intellij.openapi.Disposable;
-import com.intellij.openapi.application.PathManager;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.projectRoots.JavaSdk;
 import com.intellij.openapi.projectRoots.Sdk;
@@ -11,7 +9,6 @@ import com.intellij.openapi.roots.ModifiableRootModel;
 import com.intellij.openapi.vfs.newvfs.impl.VfsRootAccess;
 import com.intellij.pom.java.LanguageLevel;
 import com.intellij.testFramework.LightProjectDescriptor;
-import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.testFramework.PsiTestUtil;
 import com.intellij.testFramework.fixtures.DefaultLightProjectDescriptor;
 import com.intellij.testFramework.fixtures.JavaCodeInsightTestFixture;
@@ -19,17 +16,14 @@ import com.intellij.util.PathUtil;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
-import java.lang.invoke.MethodHandle;
-import java.lang.invoke.MethodHandles;
-import java.lang.invoke.MethodType;
 
 public class LombokTestUtil {
   private static final String LOMBOK_LIBRARY_DIRECTORY = "lib";
   private static final String LOMBOK_JAR_NAME = "lombok-1.18.10.jar";
 
-  public static void loadLombokLibrary(@NotNull JavaCodeInsightTestFixture projectDisposable, @NotNull Module module) {
+  public static void loadLombokLibrary(@NotNull JavaCodeInsightTestFixture fixture, @NotNull Module module) {
     final String lombokLibPath = PathUtil.toSystemIndependentName(new File(LOMBOK_LIBRARY_DIRECTORY).getAbsolutePath());
-    addLibrary(projectDisposable, module, "Lombok Library", lombokLibPath, LOMBOK_JAR_NAME);
+    addLibrary(fixture, module, "Lombok Library", lombokLibPath, LOMBOK_JAR_NAME);
   }
 
   public static LightProjectDescriptor getProjectDescriptor() {
@@ -46,31 +40,12 @@ public class LombokTestUtil {
     };
   }
 
-  @NotNull
-  public static String getTestDataPathRelativeToIdeaHome(@NotNull String relativePath) {
-    File homePath = new File(PathManager.getHomePath());
-    File testDir = new File(getTestDataRoot(), relativePath);
-
-    String relativePathToIdeaHome = FileUtil.getRelativePath(homePath, testDir);
-    if (relativePathToIdeaHome == null) {
-      throw new RuntimeException("getTestDataPathRelativeToIdeaHome: FileUtil.getRelativePath('" + homePath +
-        "', '" + testDir + "') returned null");
-    }
-
-    return relativePathToIdeaHome;
-  }
-
   /*
    * For IntelliJ >= 2017.3 we need to call PsiTestUtil.addLibrary with Disposable parameter
    * For IntelliJ <2017.3 we can use default PsiTestUtil.addLibrary version
    */
-  public static void addLibrary(JavaCodeInsightTestFixture parent, Module module, String libName, String libPath, String jarArr) {
+  private static void addLibrary(JavaCodeInsightTestFixture parent, Module module, String libName, String libPath, String jarArr) {
     VfsRootAccess.allowRootAccess(libPath);
     PsiTestUtil.addLibrary(module, libName, libPath, jarArr);
   }
-
-  public static void addLibrary(Disposable projectDisposable, Module module, String libName, String libPath, String jarArr) {
-    PsiTestUtil.addLibrary(module, libName, libPath, jarArr);
-  }
-
 }
