7552292631c44536d9fee767164aa27c4805152a
Aleksandr <lekan1992@gmail.com>
2019-10-24 20:39:35 +0300

    Fix issue #633  - @Builder.Default doesn't causes a warning message now  - partly rewrite LombokHighlightErrorFilter and its handler-utility classes  - added test for issue 633  - replace class.getName().replace("$", ".") onto class.getCanonicalName()

--- src/main/java/de/plushnikov/intellij/plugin/handler/BuilderHandler.java
+++ src/main/java/de/plushnikov/intellij/plugin/handler/BuilderHandler.java
@@ -1,17 +1,17 @@
 package de.plushnikov.intellij.plugin.handler;
 
-import com.intellij.codeInsight.daemon.impl.HighlightInfo;
+import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiField;
-import com.intellij.psi.PsiFile;
 import com.intellij.psi.util.PsiTreeUtil;
+import org.jetbrains.annotations.NotNull;
 import lombok.Builder;
 import de.plushnikov.intellij.plugin.util.PsiAnnotationSearchUtil;
 
 
 public class BuilderHandler {
 
-  public static boolean isDefaultBuilderValue(HighlightInfo highlightInfo, PsiFile file) {
-    PsiField field = PsiTreeUtil.getParentOfType(file.findElementAt(highlightInfo.getStartOffset()), PsiField.class);
+  public static boolean isDefaultBuilderValue(@NotNull PsiElement highlightedElement) {
+    PsiField field = PsiTreeUtil.getParentOfType(highlightedElement, PsiField.class);
     if (field == null) {
       return false;
     }
