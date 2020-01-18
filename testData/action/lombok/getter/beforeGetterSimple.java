138945198e8d88a0bde282ceb8e7a7fb6add4ac7
Roland Illig <roland.illig@gmx.de>
2020-01-18 20:50:31 +0100

    Only delete getters that have the canonical form
    https://github.com/mplushnikov/lombok-intellij-plugin/issues/688

--- testData/action/lombok/getter/beforeGetterSimple.java
+++ testData/action/lombok/getter/beforeGetterSimple.java
@@ -3,6 +3,13 @@ class Test {
   private double c;
   private String d;
 
+  private int wrongReturnType;
+  private int wrongField;
+  private int fieldWithoutGetter;
+  private int sideEffect;
+  private int javadoc;
+  private int comment;
+
   public float getB() {
     return b;
   }
@@ -13,5 +20,28 @@ class Test {
 
   public String getD() {
     return d;
+  }
+
+  public long getWrongReturnType() {
+    return wrongReturnType;
+  }
+
+  public int getWrongField() {
+    return wrongReturnType;
+  }
+
+  public int getSideEffect() {
+    System.out.println("side-effect");
+    return sideEffect;
+  }
+
+  /** Javadoc. */
+  public int getJavadoc() {
+    return javadoc;
+  }
+
+  public int getComment() {
+    // An implementation comment.
+    return comment;
   }
 }
