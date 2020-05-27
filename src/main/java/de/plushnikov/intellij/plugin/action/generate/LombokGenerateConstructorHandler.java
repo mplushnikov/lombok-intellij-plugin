aae5df21ac7794e477c2e062b42ae6598fdb13d1
Nikita Katkov <Nikita.Katkov@jetbrains.com>
2020-05-27 15:31:09 +0300

    Do not use ImplicitUsageProvider in GenerateConstructorAction: see [ IDEA-228014]

--- src/main/java/de/plushnikov/intellij/plugin/action/generate/LombokGenerateConstructorHandler.java
+++ src/main/java/de/plushnikov/intellij/plugin/action/generate/LombokGenerateConstructorHandler.java
@@ -1,16 +1,10 @@
 package de.plushnikov.intellij.plugin.action.generate;
 
-import com.intellij.codeInsight.daemon.ImplicitUsageProvider;
-import com.intellij.codeInsight.generation.ClassMember;
-import com.intellij.codeInsight.generation.GenerateConstructorHandler;
-import com.intellij.codeInsight.generation.GenerationInfo;
-import com.intellij.codeInsight.generation.PsiFieldMember;
-import com.intellij.codeInsight.generation.PsiGenerationInfo;
+import com.intellij.codeInsight.generation.*;
 import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiField;
 import com.intellij.psi.PsiModifier;
 import com.intellij.util.IncorrectOperationException;
-import de.plushnikov.intellij.plugin.provider.LombokImplicitUsageProvider;
 import de.plushnikov.intellij.plugin.psi.LombokLightMethodBuilder;
 import org.jetbrains.annotations.NotNull;
 
@@ -21,13 +15,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LombokGenerateConstructorHandler extends GenerateConstructorHandler {
-
   @Override
   protected ClassMember[] getAllOriginalMembers(PsiClass aClass) {
     PsiField[] fields = aClass.getFields();
     ArrayList<ClassMember> array = new ArrayList<>();
-    List<ImplicitUsageProvider> implicitUsageProviders = ImplicitUsageProvider.EP_NAME.getExtensionList();
-    fieldLoop:
     for (PsiField field : fields) {
       if (field.hasModifierProperty(PsiModifier.STATIC)) {
         continue;
@@ -37,11 +28,6 @@ public class LombokGenerateConstructorHandler extends GenerateConstructorHandler
         continue;
       }
 
-      for (ImplicitUsageProvider provider : implicitUsageProviders) {
-        if (!(provider instanceof LombokImplicitUsageProvider) && provider.isImplicitWrite(field)) {
-          continue fieldLoop;
-        }
-      }
       array.add(new PsiFieldMember(field));
     }
     return array.toArray(new ClassMember[0]);
