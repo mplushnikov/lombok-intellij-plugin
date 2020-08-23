4fb6008cd8b32030448aa6cc50854d03edfc898b
lihongbin <836559525@qq.com>
2020-08-23 13:26:22 +0800

    create method ok

--- src/main/java/de/plushnikov/intellij/plugin/processor/clazz/CustomJsonSerializableProcessor.java
+++ src/main/java/de/plushnikov/intellij/plugin/processor/clazz/CustomJsonSerializableProcessor.java
@@ -1,24 +1,15 @@
 package de.plushnikov.intellij.plugin.processor.clazz;
 
-import com.intellij.openapi.components.ServiceManager;
-import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.psi.*;
 import com.intellij.psi.search.GlobalSearchScope;
-import de.plushnikov.intellij.plugin.lombokconfig.ConfigKey;
 import de.plushnikov.intellij.plugin.problem.ProblemBuilder;
-import de.plushnikov.intellij.plugin.processor.LombokPsiElementUsage;
-import de.plushnikov.intellij.plugin.processor.handler.EqualsAndHashCodeToStringHandler;
-import de.plushnikov.intellij.plugin.processor.handler.EqualsAndHashCodeToStringHandler.MemberInfo;
 import de.plushnikov.intellij.plugin.psi.LombokLightMethodBuilder;
 import de.plushnikov.intellij.plugin.util.PsiClassUtil;
 import de.plushnikov.intellij.plugin.util.PsiMethodUtil;
 import lombok.JsonSerializable;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 /**
  * Inspect and validate @JsonSerializable lombok annotation on a class
@@ -31,14 +22,8 @@ public class CustomJsonSerializableProcessor extends AbstractClassProcessor {
   public static final String TO_JSON_METHOD_NAME = "toJson",
     FROM_JSON_METHOD_NAME = "fromJson";
 
-  private static final String INCLUDE_ANNOTATION_METHOD = "name";
-
   public CustomJsonSerializableProcessor() {
     super(PsiMethod.class, JsonSerializable.class);
-  }
-
-  private EqualsAndHashCodeToStringHandler getEqualsAndHashCodeToStringHandler() {
-    return ServiceManager.getService(EqualsAndHashCodeToStringHandler.class);
   }
 
   @Override
@@ -77,9 +62,7 @@ public class CustomJsonSerializableProcessor extends AbstractClassProcessor {
     if (PsiMethodUtil.hasMethodByName(classMethods, FROM_JSON_METHOD_NAME)) {
       return new ArrayList<>();
     }
-
-    final Collection<MemberInfo> memberInfos = getEqualsAndHashCodeToStringHandler().filterFields(psiClass, psiAnnotation, false, INCLUDE_ANNOTATION_METHOD);
-    final PsiMethod stringMethod = createToStringMethod(psiClass, memberInfos, psiAnnotation, false);
+    final PsiMethod stringMethod = fromJsonStringMethod(psiClass);
     return Collections.singletonList(stringMethod);
   }
 
@@ -89,96 +72,31 @@ public class CustomJsonSerializableProcessor extends AbstractClassProcessor {
     if (PsiMethodUtil.hasMethodByName(classMethods, TO_JSON_METHOD_NAME)) {
       return new ArrayList<>();
     }
-
-    final Collection<MemberInfo> memberInfos = getEqualsAndHashCodeToStringHandler().filterFields(psiClass, psiAnnotation, false, INCLUDE_ANNOTATION_METHOD);
-    final PsiMethod stringMethod = createToStringMethod(psiClass, memberInfos, psiAnnotation, false);
+    final PsiMethod stringMethod = toJsonStringMethod(psiClass);
     return Collections.singletonList(stringMethod);
   }
 
-  @NotNull
-  public PsiMethod createToStringMethod(@NotNull PsiClass psiClass, @NotNull Collection<MemberInfo> memberInfos, @NotNull PsiAnnotation psiAnnotation, boolean forceCallSuper) {
+  private PsiMethod toJsonStringMethod(@NotNull PsiClass psiClass) {
     final PsiManager psiManager = psiClass.getManager();
-
-    final String paramString = createParamString(psiClass, memberInfos, psiAnnotation, forceCallSuper);
-    final String blockText = String.format("return \"%s(%s)\";", getSimpleClassName(psiClass), paramString);
-
     final LombokLightMethodBuilder methodBuilder = new LombokLightMethodBuilder(psiManager, TO_JSON_METHOD_NAME)
       .withMethodReturnType(PsiType.getJavaLangString(psiManager, GlobalSearchScope.allScope(psiClass.getProject())))
       .withContainingClass(psiClass)
-      .withNavigationElement(psiAnnotation)
       .withModifier(PsiModifier.PUBLIC);
+    methodBuilder.withBody(PsiMethodUtil.createCodeBlockFromText("return com.alibaba.fastjson.JSON.toJSONString(this)", methodBuilder));
+    return methodBuilder;
+  }
+
+  private PsiMethod fromJsonStringMethod(@NotNull PsiClass psiClass) {
+    final PsiManager psiManager = psiClass.getManager();
+    final LombokLightMethodBuilder methodBuilder = new LombokLightMethodBuilder(psiManager, FROM_JSON_METHOD_NAME)
+      .withMethodReturnType(PsiType.getTypeByName(Objects.requireNonNull(psiClass.getQualifiedName()), psiManager.getProject(), GlobalSearchScope.allScope(psiClass.getProject())))
+      .withContainingClass(psiClass)
+      .withParameter("jsonString", PsiType.getJavaLangString(psiManager, GlobalSearchScope.allScope(psiClass.getProject())))
+      .withModifier(PsiModifier.PUBLIC)
+      .withModifier(PsiModifier.STATIC);
+    String blockText = String.format("return com.alibaba.fastjson.JSON.parseObject(jsonString,%s)", psiClass.getQualifiedName());
     methodBuilder.withBody(PsiMethodUtil.createCodeBlockFromText(blockText, methodBuilder));
     return methodBuilder;
   }
 
-  private String getSimpleClassName(@NotNull PsiClass psiClass) {
-    final StringBuilder psiClassName = new StringBuilder();
-
-    PsiClass containingClass = psiClass;
-    do {
-      if (psiClassName.length() > 0) {
-        psiClassName.insert(0, '.');
-      }
-      psiClassName.insert(0, containingClass.getName());
-      containingClass = containingClass.getContainingClass();
-    } while (null != containingClass);
-
-    return psiClassName.toString();
-  }
-
-  private String createParamString(@NotNull PsiClass psiClass, @NotNull Collection<MemberInfo> memberInfos, @NotNull PsiAnnotation psiAnnotation, boolean forceCallSuper) {
-    final boolean callSuper = forceCallSuper || readCallSuperAnnotationOrConfigProperty(psiAnnotation, psiClass, ConfigKey.TOSTRING_CALL_SUPER);
-    final boolean doNotUseGetters = readAnnotationOrConfigProperty(psiAnnotation, psiClass, "doNotUseGetters", ConfigKey.TOSTRING_DO_NOT_USE_GETTERS);
-    final boolean includeFieldNames = readAnnotationOrConfigProperty(psiAnnotation, psiClass, "includeFieldNames", ConfigKey.TOSTRING_INCLUDE_FIELD_NAMES);
-
-    final StringBuilder paramString = new StringBuilder();
-    if (callSuper) {
-      paramString.append("super=\" + super.toString() + \", ");
-    }
-
-    for (MemberInfo memberInfo : memberInfos) {
-
-      if (includeFieldNames) {
-        paramString.append(memberInfo.getName()).append('=');
-      }
-      paramString.append("\"+");
-
-      final PsiType classFieldType = memberInfo.getType();
-      if (classFieldType instanceof PsiArrayType) {
-        final PsiType componentType = ((PsiArrayType) classFieldType).getComponentType();
-        if (componentType instanceof PsiPrimitiveType) {
-          paramString.append("java.util.Arrays.toString(");
-        } else {
-          paramString.append("java.util.Arrays.deepToString(");
-        }
-      }
-
-      final String memberAccessor = getEqualsAndHashCodeToStringHandler().getMemberAccessorName(memberInfo, doNotUseGetters, psiClass);
-      paramString.append("this.").append(memberAccessor);
-
-      if (classFieldType instanceof PsiArrayType) {
-        paramString.append(")");
-      }
-
-      paramString.append("+\", ");
-    }
-    if (paramString.length() > 2) {
-      paramString.delete(paramString.length() - 2, paramString.length());
-    }
-    return paramString.toString();
-  }
-
-
-  @Override
-  public LombokPsiElementUsage checkFieldUsage(@NotNull PsiField psiField, @NotNull PsiAnnotation psiAnnotation) {
-    final PsiClass containingClass = psiField.getContainingClass();
-    if (null != containingClass) {
-      final String psiFieldName = StringUtil.notNullize(psiField.getName());
-      if (getEqualsAndHashCodeToStringHandler().filterFields(containingClass, psiAnnotation, false, INCLUDE_ANNOTATION_METHOD).stream()
-        .map(MemberInfo::getName).anyMatch(psiFieldName::equals)) {
-        return LombokPsiElementUsage.READ;
-      }
-    }
-    return LombokPsiElementUsage.NONE;
-  }
 }
