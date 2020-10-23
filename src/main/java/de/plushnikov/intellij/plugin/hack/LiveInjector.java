dbbf0f262e1424e6a28759fc7b05233c35f9aee2
NekoCaffeine <nekocaffeine@qq.com>
2020-10-24 03:54:45 +0800

    cleanup

--- src/main/java/de/plushnikov/intellij/plugin/hack/LiveInjector.java
+++ src/main/java/de/plushnikov/intellij/plugin/hack/LiveInjector.java
@@ -4,13 +4,13 @@ import java.lang.instrument.Instrumentation;
 
 public class LiveInjector {
 
-    public static /*@Nullable*/ String agentArgs;
+  public static /*@Nullable*/ String agentArgs;
 
-    public static /*@Nullable*/ Instrumentation instrumentation;
+  public static /*@Nullable*/ Instrumentation instrumentation;
 
-    public static void agentmain(final String agentArgs, final Instrumentation instrumentation) {
-        LiveInjector.agentArgs = agentArgs;
-        LiveInjector.instrumentation = instrumentation;
-    }
+  public static void agentmain(final String agentArgs, final Instrumentation instrumentation) {
+    LiveInjector.agentArgs = agentArgs;
+    LiveInjector.instrumentation = instrumentation;
+  }
 
 }
